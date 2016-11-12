e01df2a3e9aeee392d6fae30f77fddbe28adb9fd
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-11-12 06:38:25 -0800

    Start with specific commit

--- WaitForDbCreaionBuild.ps1
+++ WaitForDbCreaionBuild.ps1
@@ -1,4 +1,11 @@
     param (
+                
+        [Parameter(Mandatory=$true)]
+        [string]$JobName,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$masterCategory,
+        
         [Parameter(Mandatory=$true)]
         [string]$AccountName,
         
@@ -7,6 +14,9 @@
         
         [Parameter(Mandatory=$true)]
         [string]$ApiKey,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$branch,
         
         [Parameter(Mandatory=$true)]
         [string]$RepoCommit,
@@ -22,22 +32,43 @@
       "Content-type" = "application/json"
     }
     
-    [datetime]$stop = ([datetime]::Now).AddMinutes($TimeOutMins)
-    [bool]$dbCreated = $false
-    
-    while(!$dbCreated -and ([datetime]::Now) -lt $stop) {
-    
-        $project = Invoke-RestMethod -Uri "https://ci.appveyor.com/api/projects/$AccountName/$ProjectSlug" -Headers $headers -Method GET
-        Write-host "Last create DB project commit:"
-        $project.build.commitId
-        Write-host "Last create DB project status:"
-        $project.build.status
-        $dbCreated = ($project.build.commitId -eq $RepoCommit) -and ($project.build.status -eq "success")
-        if (!$dbCreated) {
-            Start-sleep 5
+    function StartBuild {
+       	$body = @{
+            accountName=$AccountName
+            projectSlug=$ProjectSlug
+            branch=$branch
+            commitId=$RepoCommit
         }
+        
+        $body = $body | ConvertTo-Json
+	
+        Invoke-RestMethod -Uri "https://ci.appveyor.com/api/builds" -Headers $headers -Body $body -Method POST                  
     }
     
-    if (!$dbCreated) {
-        throw "DB was not created in $TimeOutMins minutes"
+    function Wait {
+        [datetime]$stop = ([datetime]::Now).AddMinutes($TimeOutMins)
+        [bool]$dbCreated = $false
+        
+        while(!$dbCreated -and ([datetime]::Now) -lt $stop) {
+        
+            $project = Invoke-RestMethod -Uri "https://ci.appveyor.com/api/projects/$AccountName/$ProjectSlug" -Headers $headers -Method GET
+            Write-host "Last create DB project commit:"
+            $project.build.commitId
+            Write-host "Last create DB project status:"
+            $project.build.status
+            $dbCreated = ($project.build.commitId -eq $RepoCommit) -and ($project.build.status -eq "success")
+            if (!$dbCreated) {
+                Start-sleep 5
+            }
+        }
+        
+        if (!$dbCreated) {
+            throw "DB was not created in $TimeOutMins minutes"
+        }    
     }
+    
+    if ($JobName.Contains($masterCategory)) {
+        StartBuild;
+    }
+    Wait;
+
