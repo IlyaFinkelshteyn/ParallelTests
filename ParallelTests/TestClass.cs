bf64cb6af6d6b42dde2ad07ab1d0b9d443b69ae2
IlyaFinkelshteyn <ilyaf@appveyor.com>
2016-11-12 02:13:10 -0800

    Initial commit

---
+++ ParallelTests/TestClass.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        [Category("C1")]
+        public void TestMethod1()
+        {
+            Assert.Pass("Your first passing test");
+        }
+
+        [Test]
+        [Category("C2")]
+        public void TestMethod2()
+        {
+            Assert.Pass("Your first passing test");
+        }
+
+        [Test]
+        [Category("C3")]
+        public void TestMethod3()
+        {
+            Assert.Pass("Your first passing test");
+        }
+        [Test]
+        [Category("C4")]
+
+        public void TestMethod4()
+        {
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
